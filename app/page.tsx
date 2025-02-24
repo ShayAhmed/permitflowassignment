5a4f40a81a7f783a589d5d89a0fe599a3f23b24f
ShayAhmed <sasheroo@gmail.com>
2025-02-24 15:04:45 -0500

    initial commit, add boilerplate code

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="flex flex-col gap-2 max-w-6xl ml-auto py-4">
+        <h1 className="text-3xl">PermitFlow</h1>
+        <span className="italic">Your solution goes here.</span>
+      </div>
+    </main>
+  )
+}
